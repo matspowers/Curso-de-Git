79f93dc023915b7f180b82a936f5f1cea4d5e6ec
Mats Powers <mats.powers@gmail.com>
2016-06-09 14:36:11 -0500

    se agrego una impresion en pantalla

--- ejemplo.java
+++ ejemplo.java
@@ -1,5 +1,10 @@
 import java.lang.io;
+import java.lang.String;
 
 public void public static void main(String[] args) {
-	
+	system.out.println("hola mundo");
 }
+
+
+//  Desgraciadamente este archivo no hace nada
+
